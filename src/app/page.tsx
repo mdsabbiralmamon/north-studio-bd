e15b1ebfb250cf61e96fa36698767af1482ee7bc
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-06-12 19:50:01 +0600

    Modified
    Add: about us section
    remove: trial code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
+import AboutSection from "@/components/HomePage/AboutSection/AboutSection";
 import Hero from "@/components/HomePage/Hero/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <div className="bg-blue-300 h-[600px]">
-        <h1>Hello World</h1>
+      <div className="about container mx-auto">
+        <AboutSection />
       </div>
     </>
   );
