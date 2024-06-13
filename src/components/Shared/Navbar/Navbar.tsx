5cc4c9a8fd797e1368b168bfccc86016cf849b4a
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-06-13 23:23:26 +0600

    updated
    nav links updated

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -22,6 +22,14 @@ export default function Navbar() {
         };
     }, []);
 
+    const navLinks = <>
+        <li><a>Home</a></li>
+        <li><a>Architectural</a></li>
+        <li><a>Portfolio</a></li>
+        <li><a>About</a></li>
+        <li><a>Services</a></li>
+    </>
+
     return (
         <div className={`navbar fixed ${isScrolled ? 'bg-white border-b border-b-gray-300' : 'transparent border-b'} z-10`}>
             <div className="navbar-start">
@@ -30,7 +38,9 @@ export default function Navbar() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Home</a></li>
+                        {
+                            navLinks
+                        }
                     </ul>
                 </div>
                 <div className='flex gap-2 justify-center items-center'>
@@ -42,7 +52,9 @@ export default function Navbar() {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Home</a></li>
+                    {
+                        navLinks
+                    }
                 </ul>
             </div>
             <div className="navbar-end">
