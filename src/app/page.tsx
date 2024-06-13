acb200318ec445266980c0aeaf9b2e163a91df7c
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-06-14 04:16:04 +0600

    updated
    added advantage section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutSection from "@/components/HomePage/AboutSection/AboutSection";
 import Hero from "@/components/HomePage/Hero/Hero";
+import OurAdvantageSection from "@/components/HomePage/OurAdvantageSection/OurAdvantageSection";
 import OurPurpose from "@/components/HomePage/OurPurpose/OurPurpose";
 import ServicesSection from "@/components/HomePage/ServicesSection/ServicesSection";
 
@@ -10,11 +11,14 @@ export default function Home() {
       <div className="about container mx-auto">
         <AboutSection />
       </div>
-      <div className="about container mx-auto my-24">
+      <div className="ourPurpose container mx-auto my-24">
         <OurPurpose />
       </div>
-      <div className="about container mx-auto my-24">
+      <div className="services container mx-auto my-24">
         <ServicesSection />
+      </div>
+      <div className="advantages container mx-auto my-24">
+        <OurAdvantageSection />
       </div>
     </>
   );
