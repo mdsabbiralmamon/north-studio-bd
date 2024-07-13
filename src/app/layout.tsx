c3bbef68b4981c2280637dc7e8d3722c809bc6dd
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-07-13 15:33:55 +0600

    Updated: added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Shared/Navbar/Navbar";
+import Footer from "@/components/Shared/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +26,7 @@ export default function RootLayout({
           {children}
         </main>
         <footer>
-
+          <Footer />
         </footer>
       </body>
     </html>
