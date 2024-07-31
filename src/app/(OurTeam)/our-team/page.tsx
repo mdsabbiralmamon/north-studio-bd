bf64c98418e0416b97ffb92ea159319a4b4f7f33
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-07-31 23:25:08 +0600

    Updated: Add North Studio Managing Directors and Core Members to Our Team page(Demo data)

--- src/app/(OurTeam)/our-team/page.tsx
+++ src/app/(OurTeam)/our-team/page.tsx
@@ -13,8 +13,18 @@ export default function page() {
                     <p className='text-base font-normal lowercase first-letter:uppercase max-w-2xl mx-auto my-4'>We are a team of Architects, Planners, Creatives and forward-thinking individuals.</p>
                 </div>
                 <div className='my-24'>
-                    <h2 className='customTextBorder p-2 text-3xl font-bold mb-12'>North Studio Family Members:</h2>
-                    <div>
+                    <h2 className='customTextBorder p-2 text-3xl font-bold mb-12'>North Studio Managing Directors:</h2>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                    </div>
+                    <h2 className='customTextBorder p-2 text-3xl font-bold mt-24 mb-12'>North Studio Core Members:</h2>
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
                         <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
                     </div>
                 </div>
