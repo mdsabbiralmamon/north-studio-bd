acc95bd395adfe949f011f1ddde24f27b25ad01b
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-01 18:22:07 +0600

    feat: Add team members' images and update Our Team page
    This commit adds the images of team members to the Our Team page. It also updates the page to include the North Studio Managing Directors and Core Members as demo data. Additionally, the CEO is added to the North Studio Family Members section. The TeamCard component is created and added to the page.
    
    Fixes : default data

--- src/app/(OurTeam)/our-team/page.tsx
+++ src/app/(OurTeam)/our-team/page.tsx
@@ -3,6 +3,16 @@ import React from 'react'
 import bannerImg from '@/app/images/sliderImages/interior1.jpg'
 import TeamCard from '@/components/Shared/TeamCard/TeamCard'
 
+// team images
+import defaultFemale from '@/app/images/team/default-female.jpg';
+import defaultMale from '@/app/images/team//deafault-male.png';
+import rifat from '@/app/images/team/RIFAT.jpg';
+import arafat from '@/app/images/team/Arafat.jpg';
+import pranto from '@/app/images/team/PRANTO-2.jpg';
+import rithim from '@/app/images/team/RITHIM.jpg';
+import orny from '@/app/images/team/Orny.jpg';
+import fahmi from '@/app/images/team/Fahmida.jpg';
+
 export default function page() {
     return (
         <div>
@@ -14,18 +24,20 @@ export default function page() {
                 </div>
                 <div className='my-24'>
                     <h2 className='customTextBorder p-2 text-3xl font-bold mb-12'>North Studio Managing Directors:</h2>
-                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
-                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
-                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
-                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
-                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                    <div className='grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-fit mx-auto'>
+                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={rifat.src} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Arafat Hossain'} image={arafat.src} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Najmus Sakib Pranto'} image={pranto.src} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
                     </div>
                     <h2 className='customTextBorder p-2 text-3xl font-bold mt-24 mb-12'>North Studio Core Members:</h2>
-                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
-                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
-                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
-                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
-                        <TeamCard name={'Rifat Ul Zaka Rakib(CEO)'} image={'https://northstudiobd.com/wp-content/uploads/2023/06/RIFAT.jpg'} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                    <div className='grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-fit mx-auto'>
+                        <TeamCard name={'Rithim Noor'} image={rithim.src} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Orny Sumaiya'} image={orny.src} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Rabaya Taposhi Irany'} image={defaultFemale.src} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Sadin Jerin Anamika'} image={defaultFemale.src} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Fahmida Marium'} image={fahmi.src} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Md. Shourov'} image={defaultMale.src} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
+                        <TeamCard name={'Tahrafi Sakib Faisal'} image={defaultMale.src} facebook={'#'} instagram={'#'} twitter={'#'} youtube={'#'} />
                     </div>
                 </div>
             </div>
