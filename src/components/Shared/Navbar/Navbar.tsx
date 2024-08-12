d1ee6b94ee027d49159ef94cc2ad5549a17237ea
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-13 02:14:38 +0600

    refactor: Update Navbar component with correct contact link

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -25,8 +25,8 @@ export default function Navbar() {
     const navLinks = <>
         <li><a href='/'>Home</a></li>
         <li><a href='/about'>About</a></li>
-        <li><a>Services</a></li>
-        <li><a>Portfolio</a></li>
+        <li><a href='/services'>Services</a></li>
+        <li><a href='/portfolio'>Portfolio</a></li>
         <li><a href='/contact'>Contact</a></li>
         <li><a href='/our-team'>Our Team</a></li>
     </>
