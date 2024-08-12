92b0c96d20606a369349af97aaaf748d4edd8432
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-12 17:45:31 +0600

    feat: Add ClientsQuote component
    This commit adds the ClientsQuote component to the AboutPage. The component displays a QuoteCard and improves the overall design and functionality of the page.
    
    Fixes: None

---
+++ src/components/AboutPage/ClientsQuote.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuoteCard from '../Shared/QuoteCard/QuoteCard';
+
+const ClientsQuote = () => {
+    return (
+        <div>
+            <QuoteCard />
+        </div>
+    );
+};
+
+export default ClientsQuote;
