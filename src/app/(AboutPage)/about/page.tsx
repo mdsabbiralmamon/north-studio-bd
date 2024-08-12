6a31de143991c16a3684ba80c1a71d9a0b61cb55
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-12 17:45:18 +0600

    refactor: Update About page with new components and styles
    This commit refactors the About page by adding new components and styles. The page now includes a Details card component, a Philosophy component, and an About Us component. The navigation links have also been modified. These changes improve the overall design and functionality of the About page.
    
    Fixes: None

--- src/app/(AboutPage)/about/page.tsx
+++ src/app/(AboutPage)/about/page.tsx
@@ -1,19 +1,35 @@
-import Banner from '@/components/Shared/Banner/Banner';
 import React from 'react';
+import Banner from '@/components/Shared/Banner/Banner';
 import bannerImg from '@/app/images/sliderImages/exterior1.jpg';
 import AboutUs from '@/components/AboutPage/AboutUs';
 import OurPhilosophy from '@/components/AboutPage/OurPhilosophy';
+import Parallax from '@/components/Shared/Parallax/Parallax';
+import { FaHome } from 'react-icons/fa';
+import { BsFillHouseAddFill, BsFillHouseGearFill, BsFillPeopleFill } from 'react-icons/bs';
+import ClientsQuote from '@/components/AboutPage/ClientsQuote';
 
-export default function page() {
-    return (
-        <div>
-            <Banner bannerImg={bannerImg} banner_title='ABOUT US' />
-            <div className='container mx-auto my-24'>
-                <AboutUs />
-            </div>
-            <div className='container mx-auto my-24'>
-                <OurPhilosophy />
-            </div>
-        </div>
-    )
+export default function Page() {
+  return (
+    <div>
+      <Banner bannerImg={bannerImg} banner_title='ABOUT US' />
+      <div className='container mx-auto my-24'>
+        <AboutUs />
+      </div>
+      <div className='container mx-auto my-24'>
+        <OurPhilosophy />
+      </div>
+      <div className='my-24'>
+      <Parallax
+        backgroundImage={bannerImg.src}
+        info1={{ icon: <FaHome className='text-8xl' />, count: '750', title: 'Home Interior renovations' }}
+        info2={{ icon: <BsFillHouseGearFill className='text-8xl' />, count: '300', title: 'House re-modelling' }}
+        info3={{ icon: <BsFillHouseAddFill className='text-8xl' />, count: '120', title: 'Commercial rentals' }}
+        info4={{ icon: <BsFillPeopleFill className='text-8xl' />, count: '1000', title: 'Happy Clients' }}
+      />
+      </div>
+      <div className='my-24'>
+        <ClientsQuote />
+      </div>
+    </div>
+  );
 }
