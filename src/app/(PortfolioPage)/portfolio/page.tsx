cceaf747802d793998392fdf99097f8afce232bb
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-13 02:21:20 +0600

    feat: Add Banner component to portfolio page
    This commit adds the Banner component to the portfolio page, enhancing the visual appeal and user experience of the website.
    
    Fixes: None

--- src/app/(PortfolioPage)/portfolio/page.tsx
+++ src/app/(PortfolioPage)/portfolio/page.tsx
@@ -1,9 +1,12 @@
+import Banner from '@/components/Shared/Banner/Banner';
 import React from 'react';
+import bannerImg from '@/app/images/sliderImages/building1.jpg';
+
 
 const page = () => {
     return (
         <div>
-            portfolio page
+            <Banner bannerImg={bannerImg} banner_title='PORTFOLIO' />
         </div>
     );
 };
