6c190d2910313e7ec328949bf7a7a67888b5edb5
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-12 18:10:14 +0600

    refactor: Update Navbar component with correct contact link
    The Navbar component has been updated to include the correct link for the Contact page. Previously, the link was missing the href attribute, which prevented users from accessing the Contact page. This change improves the navigation functionality and ensures a seamless user experience.
    
    Fixes: None

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -27,7 +27,7 @@ export default function Navbar() {
         <li><a href='/about'>About</a></li>
         <li><a>Services</a></li>
         <li><a>Portfolio</a></li>
-        <li><a>Contact</a></li>
+        <li><a href='/contact'>Contact</a></li>
         <li><a href='/our-team'>Our Team</a></li>
     </>
 
