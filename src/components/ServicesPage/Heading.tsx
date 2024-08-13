b970f23417c80566dc3428a5ef3920c316fd8ae1
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-13 12:17:32 +0600

    feat: Add Heading component to ServicesPage
    This commit adds the Heading component to the ServicesPage, improving the content and visual appeal of the services page.
    
    Fixes: None

---
+++ src/components/ServicesPage/Heading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Heading = () => {
+    return (
+        <div className='text-center'>
+            <h2 className='customTextBorder p-2 text-xl font-bold uppercase'>Modern concept</h2>
+            <p className='my-8'>Ruizarch we are a leader in architecture and design with a user-centric approach and deep technical expertise. We are consistently ranked as one of the top 10 architecture firms in the world, and our core disciplines are supported by a wide range of complementary services, all of which have a singular goal: to build environments where our clients thrive.</p>
+        </div>
+    );
+};
+
+export default Heading;
