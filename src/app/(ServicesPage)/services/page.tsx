10f081f541a77ecd3d75aaf4fcb1c1fedd220bd2
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-08-13 12:17:13 +0600

    feat: Add Heading and OurServices components to ServicesPage
    This commit adds the Heading and OurServices components to the ServicesPage, enhancing the content and user experience of the services page.
    
    Fixes: None

--- src/app/(ServicesPage)/services/page.tsx
+++ src/app/(ServicesPage)/services/page.tsx
@@ -1,11 +1,19 @@
 import Banner from '@/components/Shared/Banner/Banner';
 import React from 'react';
 import bannerImg from '@/app/images/sliderImages/interior1.jpg';
+import Heading from '@/components/ServicesPage/Heading';
+import OurServices from '@/components/ServicesPage/OurServices';
 
 const page = () => {
     return (
         <div>
             <Banner bannerImg={bannerImg} banner_title='SERVICES' />
+            <div className='my-24 container mx-auto'>
+                <Heading />
+            </div>
+            <div className='my-24 container mx-auto'>
+                <OurServices />
+            </div>
         </div>
     );
 };
