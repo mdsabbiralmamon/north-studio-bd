ae649f5b4f7955786afec00b3dcbb732856cf67e
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-10-12 18:49:12 +0600

    Refactor Banner component to update banner title styling

--- src/components/Shared/Banner/Banner.tsx
+++ src/components/Shared/Banner/Banner.tsx
@@ -9,7 +9,7 @@ const Banner: React.FC<BannerProps> = (props) => {
     const { banner_title, bannerImg } = props;
 
     const bannerStyle: React.CSSProperties = {
-        backgroundImage: `linear-gradient(180deg, rgba(3, 7, 18, 0.90) 0%, rgba(3, 7, 18, 0.50) 100%), url(${bannerImg.src})`,
+        backgroundImage: `linear-gradient(180deg, rgba(3, 7, 18, 0.90) 0%, rgba(3, 7, 18, 0.50) 100%), url(${bannerImg.src|| bannerImg})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
@@ -17,7 +17,7 @@ const Banner: React.FC<BannerProps> = (props) => {
 
     return (
         <div>
-            <h2 className='h-[545px] text-white flex justify-center items-center text-5xl font-extrabold' style={bannerStyle}>{banner_title}</h2>
+            <h2 className='h-[545px] text-white flex justify-center items-center text-5xl font-extrabold uppercase' style={bannerStyle}>{banner_title}</h2>
         </div>
     );
 };
