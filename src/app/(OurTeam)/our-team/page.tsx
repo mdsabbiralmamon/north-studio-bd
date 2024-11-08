7c5e36d71902376fa3f9374d7c2c7b13cd576f0d
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-11-08 14:43:16 +0600

    Refactor team page to update team members' information and improve layout

--- src/app/(OurTeam)/our-team/page.tsx
+++ src/app/(OurTeam)/our-team/page.tsx
@@ -1,47 +1,58 @@
-import Banner from '@/components/Shared/Banner/Banner'
-import React from 'react'
-import bannerImg from '@/app/images/sliderImages/interior1.jpg'
-import TeamCard from '@/components/Shared/TeamCard/TeamCard'
-
-// team images
-import defaultFemale from '@/app/images/team/default-female.jpg';
-import defaultMale from '@/app/images/team//deafault-male.png';
-// import rifat from '@/app/images/team/RIFAT.jpg';
-// import arafat from '@/app/images/team/Arafat.jpg';
-// import pranto from '@/app/images/team/PRANTO-2.jpg';
+import Banner from '@/components/Shared/Banner/Banner';
+import React from 'react';
+import bannerImg from '@/app/images/sliderImages/interior1.jpg';
 
 export default function page() {
     return (
         <div>
-            <Banner bannerImg={bannerImg} banner_title='OUR TEAM' />
-            <div className='container mx-auto '>
-                <div className='my-24 text-center'>
-                    <h2 className='customTextBorder p-2 text-5xl font-bold'>Meet Our Team</h2>
-                    <p className='text-base font-normal max-w-2xl mx-auto my-4'>We are a team of Architects, Interior Architects, and creative thinkers, focused on designing innovative and functional spaces that inspire.</p>
+            <Banner bannerImg={bannerImg} banner_title="OUR TEAM" />
+            <div className="container mx-auto px-4">
+                {/* Section Title */}
+                <div className="my-24 text-center">
+                    <h2 className="customTextBorder p-2 text-6xl font-extrabold text-gray-900">
+                        Meet Our Team
+                    </h2>
+                    <p className="text-lg lg:text-xl font-medium text-gray-800 max-w-2xl mx-auto my-6">
+                        We are a team of Architects, Interior Architects, and creative thinkers, focused on designing innovative and functional spaces that inspire.
+                    </p>
                 </div>
-                <div className='my-24'>
-                    <h2 className='customTextBorder p-2 text-3xl font-bold mb-12'>Partners:</h2>
-                    <div className='grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-fit mx-auto'>
-                        <TeamCard name={'Rifat Ul Zaka Rakib'} image={defaultMale.src} facebook={'https://www.facebook.com/arch.rifat'} instagram={'https://www.instagram.com/rakib.northman/'} twitter={'https://x.com/rifatarch1'} youtube={''} />
-                        <TeamCard name={'Arafat Hossain'} image={defaultMale.src} facebook={''} instagram={''} twitter={''} youtube={''} />
-                        <TeamCard name={'Najmus Sakib Pranto'} image={defaultMale.src} facebook={''} instagram={''} twitter={''} youtube={''} />
-                        <TeamCard name={'Sadia Islam Mumu'} image={defaultFemale.src} facebook={''} instagram={''} twitter={''} youtube={''} />
+
+                {/* Partners Section */}
+                <div className="my-24">
+                    <h2 className="customTextBorder p-2 text-4xl font-bold text-gray-900 mb-12">
+                        Partners
+                    </h2>
+                    <div className="grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-auto">
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Rifat Ul Zaka Rakib</p>
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Arafat Hossain</p>
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Najmus Sakib Pranto</p>
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Sadia Islam Mumu</p>
                     </div>
-                    <h2 className='customTextBorder p-2 text-3xl font-bold mt-24 mb-12'>Core Members:</h2>
-                    <div className='grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-fit mx-auto'>
-                        <TeamCard name={'Rithim Noor'} image={defaultMale.src} facebook={''} instagram={''} twitter={''} youtube={''} />
-                        <TeamCard name={'Rabaya Taposhi Irany'} image={defaultFemale.src} facebook={''} instagram={''} twitter={''} youtube={''} />
-                        <TeamCard name={'Sadin Jerin Anamika'} image={defaultFemale.src} facebook={''} instagram={''} twitter={''} youtube={''} />
+
+                    {/* Core Members Section */}
+                    <h2 className="customTextBorder p-2 text-4xl font-bold text-gray-900 mt-24 mb-12">
+                        Core Members
+                    </h2>
+                    <div className="grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-auto">
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Ar. Sania Akter</p>
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Ar. Rabaya Taposhi Irany</p>
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Sadin Jerin Anamika</p>
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Rithim Noor</p>
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Mahmudun Nabi Sourav</p>
                     </div>
-                    <h2 className='customTextBorder p-2 text-3xl font-bold mt-24 mb-12'>Away but always our family member:</h2>
-                    <div className='grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-fit mx-auto'>
-                        <TeamCard name={'Orny Sumaiya'} image={defaultFemale.src} facebook={''} instagram={''} twitter={''} youtube={''} />
-                        <TeamCard name={'Fahmida Marium'} image={defaultFemale.src} facebook={''} instagram={''} twitter={''} youtube={''} />
-                        <TeamCard name={'Md. Shourov'} image={defaultMale.src} facebook={''} instagram={''} twitter={''} youtube={''} />
-                        <TeamCard name={'Tahrafi Sakib Faisal'} image={defaultMale.src} facebook={''} instagram={''} twitter={''} youtube={''} />
+
+                    {/* Away Members Section */}
+                    <h2 className="customTextBorder p-2 text-4xl font-bold text-gray-900 mt-24 mb-12">
+                        Away but Always Our Family Member
+                    </h2>
+                    <div className="grid gap-12 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-auto">
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Orny Sumaiya</p>
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Fahmida Marium</p>
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Md. Shourov</p>
+                        <p className="text-xl font-semibold text-gray-700 hover:text-black">Tahrafi Sakib Faisal</p>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
